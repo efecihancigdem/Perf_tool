6c717ba907f4c630f3e765d3182a501b51b3dee7
efecihancigdem <cefecigdem@gmail.com>
2023-08-02 00:17:47 -0400

    Update framework.py

--- framework.py
+++ framework.py
@@ -1,19 +1,17 @@
 import paramiko
 
 HOST_NAME = '127.0.0.1'
-
 USER_NAME = 'efe'
 PASSWORD = '741049160'
 PORT = 3022
-
-command = 'hostname'
+COMMAND = 'hostname'
 
 client = paramiko.SSHClient()
 
 client.load_system_host_keys()
 client.connect(hostname=HOST_NAME, port=PORT, username=USER_NAME, password=PASSWORD)
 
-(stdin, stdout, stderr) = client.exec_command(command)
+(stdin, stdout, stderr) = client.exec_command(COMMAND)
 
 output = stdout.read()
 print(str(output, 'utf8'))
